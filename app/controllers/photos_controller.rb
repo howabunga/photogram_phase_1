9e224f40f99a1dd3b4d63201e712d540e200ee9b
howabunga <weihowa@gmail.com>
2015-07-29 12:55:33 -0500

    Progress during lunch
    Commit/Sync before trying out rake db:seed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,53 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+
+    @show = params["id"]
+
+    render("show.html.erb")
+
+  end
+
+  def edit
+
+    @edit = params["id"]
+
+    render("edit.html.erb")
+
+    Photo.find({ :id => @edit })
+    Photo.caption = "#{@caption}"
+    Photo.save
+
+    Photo.find({ :id => @edit })
+    Photo.caption = "#{@source}"
+    Photo.save
+
+    redirect_to("/photos")
+
+  end
+
+  def destroy
+
+    @destroy = params["id"]
+
+    Photo.find_by({ :id => @destroy }).destroy
+
+    redirect_to("/photos")
+
+  end
+
+  def new
+
+    render("new.html.erb")
+
+    p = Photo.new
+    p.caption = @caption
+    p.source = @source
+    p.save
+
+    redirect_to("/photos")
+
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("photos/:id/edit", { :controller => "photos", :action => "edit"})
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+  get("/photos/new", { :controller => "photos", :action => "new"})
 
 end
